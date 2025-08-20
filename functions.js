d1be1cbdfe373d9e883bbea6700c0e61ca536053
Mukilan-Uki <mukilan.uki.stu@dreamspace.academy>
2025-08-20 15:01:10 +0530

    printName line

---
+++ functions.js
@@ -0,0 +1,18 @@
+var name1 = "Jack";
+var name2 = "Sparrow";
+var name3 = "John";
+var name4 = "Doe";
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
+console.log(name4);
+
+function printName(){
+    console.log("Hello "+name1);
+    console.log("Hello "+name2);
+    console.log("Hello "+name3);
+    console.log("Hello "+name4);
+}
+
+printName();
