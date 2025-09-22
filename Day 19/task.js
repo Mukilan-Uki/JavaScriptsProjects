43b8aac81582b6e2068ae1d2c40b9df4d52086f4
Mukilan-Uki <mukilan.uki.stu@dreamspace.academy>
2025-09-22 16:45:35 +0530

    day 19

---
+++ Day 19/task.js
@@ -0,0 +1,37 @@
+let row=4;
+let col=3;
+let matrix=generate2DArray(row,col)
+function generate2DArray(row,col){
+    let mat=Array.from({length:row}, ()=>new Array(col).fill(0));
+    for(let i=0; i<row; i++){
+        for(let j=0; j<col; j++){
+            mat[i][j]=Math.floor(Math.random()*20);
+        }
+    }
+    return mat;
+}
+
+function printArray(mat){
+    for(let i=0; i<mat.length; i++){
+        console.log(mat[i]);
+    }
+}
+printArray(matrix);
+
+
+function reverse(mat){
+    let revmat=Array.from({length:matrix[0].length},()=>new Array(matrix.length).fill(0))
+    for(let i=0; i<mat.length; i++){
+        for(let j=0; j<mat[0].length; j++){
+            revmat[j][i]=mat[i][j];
+        }
+    }
+    return revmat;
+}
+console.log(reverse(matrix))
+
+let revmatrix=reverse(matrix);
+
+printArray(revmatrix);
+
+
